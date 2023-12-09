8dd618a02e6aa65f52d750a08dc54214485bd7b3
Yousuf Abdullah Sakib <141171865+yousuf-abdullah-sakib@users.noreply.github.com>
2023-12-09 14:58:39 +0600

    Add files via upload

---
+++ asset/js/custom.js
@@ -0,0 +1,426 @@
+(function($){
+    'use strict';
+
+
+    $('.all-bid').on('click', function(){
+
+        $('.sure').show();
+
+    });
+   
+   
+
+
+
+
+
+
+    
+    $('.location').on('click', function(){
+
+        $('.loc-area').slideToggle();
+
+    });
+
+    
+    $('.bajed').on('click', function(){
+
+        $('.pop-up-2-b').slideToggle();
+    });
+    $('.pop-up-2-as').on('click', function(){
+
+        $('.pop-up-2-b').slideToggle();
+    });
+
+
+    $('.dtime').on('click', function(){
+
+        $('.pop-up-d').slideToggle();
+    });
+    $('.pop-up-2-c').on('click', function(){
+
+        $('.pop-up-d').slideToggle();
+    });
+
+
+
+    $('.rating').on('click', function(){
+
+        $('.w-pop-up-1').slideToggle();
+    });
+    $('.rating-1').on('click', function(){
+
+        $('.w-pop-up-1').slideToggle();
+    });
+
+
+
+    
+    $('.delivary').on('click', function(){
+
+        $('.w-pop-up-2').slideToggle();
+    });
+    $('.rating-2').on('click', function(){
+
+        $('.w-pop-up-2').slideToggle();
+    });
+
+
+    $('.order-x').on('click', function(){
+
+        $('.w-pop-up-3').slideToggle();
+    });
+    $('.rating-3').on('click', function(){
+
+        $('.w-pop-up-3').slideToggle();
+    });
+    
+
+
+
+
+
+    $('.sb').on('click', function(){
+
+        $('.qn').slideToggle();
+    });
+    $('.sb-1').on('click', function(){
+
+        $('.qn-1').slideToggle();
+    });
+    $('.sb-2').on('click', function(){
+
+        $('.qn-2').slideToggle();
+    });
+
+
+    $('.sb-1').on('click', function(){
+
+        $('.qn').hide();
+    });
+    $('.sb-2').on('click', function(){
+
+        $('.qn').hide();
+    });
+
+
+    $('.sb').on('click', function(){
+
+        $('.qn-1').hide();
+    });
+    $('.sb-2').on('click', function(){
+
+        $('.qn-1').hide();
+    });
+   
+
+    
+    $('.sb').on('click', function(){
+
+        $('.qn-2').hide();
+    });
+    $('.sb-1').on('click', function(){
+
+        $('.qn-2').hide();
+    });
+
+
+
+
+
+
+
+
+
+    $('.complain').on('click', function(){
+
+        $('.pop-12').slideToggle();
+
+    });
+
+    $('.cancel').on('click', function(){
+
+        $('.pop-12').slideToggle();
+
+    });
+
+
+
+
+    $('.menu-icon i').on('click', function(){
+
+        $('.three-dot').slideToggle();
+
+    });
+
+    $('.bar-cross i').on('click', function(){
+
+        $('.three-dot').slideToggle();
+
+    });
+
+    $('.three-dot-2').on('click', function(){
+
+        $('.three-dot').slideToggle();
+
+    });
+
+
+
+
+
+
+
+
+    $('.s-icon').on('click', function(){
+
+        $('.search').slideToggle();
+
+    });
+
+    $('.s-cross-1 i').on('click', function(){
+
+        $('.search').slideToggle();
+
+    });
+
+
+
+
+
+
+
+
+    $('.db').on('click', function(){
+
+        $('.db-1').slideToggle();
+
+    });
+
+    $('.db-2').on('click', function(){
+
+        $('.db-1').slideToggle();
+
+    });
+
+
+
+
+
+
+
+    $('.forget-pass-1').on('click', function(){
+
+        $('.forget-pass').slideToggle();
+
+    });
+
+
+    $('.service-6').on('click', function(){
+
+        $('.service-3').slideToggle();
+        $('.service-6').hide();
+        $('.service-5').show();
+
+      
+    });
+
+    $('.service-5').on('click', function(){
+
+        $('.service-3').slideToggle();
+        $('.service-6').show();
+        $('.service-5').hide();
+
+      
+    });
+
+
+    $('.service-66').on('click', function(){
+
+        $('.service-33').slideToggle();
+        $('.service-55').show();
+        $('.service-66').hide();
+
+      
+    });
+
+    $('.service-55').on('click', function(){
+
+        $('.service-33').slideToggle();
+        $('.service-55').hide();
+        $('.service-66').show();
+
+      
+    });
+
+
+
+
+    $('.cn-pass').on('click', function(){
+
+        $('.write-pass').show();
+
+      
+    });
+
+    $('.cn-2').on('click', function(){
+
+        $('.write-pass').hide();
+      
+    });
+
+    $('.signout').on('click', function(){
+
+        $('.write-pass-1').show();
+      
+    });
+    $('.cn-2').on('click', function(){
+
+        $('.write-pass-1').hide();
+      
+    });
+
+
+
+
+
+    $('.colo').on('click', function(){
+
+        $('.color-pic').slideToggle();
+      
+    });
+
+    $('.colo-1').on('click', function(){
+
+        $('.color-pic').slideToggle();
+      
+    });
+
+    
+    $('.colo-2').on('click', function(){
+
+        $('.lc').slideToggle();
+      
+    });
+
+    $('.colo-2').on('click', function(){
+
+        $('.lc-1').slideToggle();
+      
+    });
+
+
+    $('.privacy-1').on('click', function(){
+
+        $('.privacy').slideToggle();
+      
+    });
+
+    $('.colo-3').on('click', function(){
+
+        $('.privacy').slideToggle();
+      
+    });
+
+
+    $('.u-1').on('click', function(){
+
+        $('.u-2').slideToggle();
+      
+    });
+    
+
+
+    $('.r-area-1').on('click', function(){
+
+        $('.r-area').slideToggle();
+      
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    $('.rep h3').on('click', function(){
+
+        $('.rep-1').slideToggle();
+        $('.rep-1').css({
+            display : 'flex'
+        });
+      
+    });
+
+
+    $('.ans h3').on('click', function(){
+
+        $('.ans-1').slideToggle();
+        $('.ans-1').css({
+            display : 'flex'
+        });
+      
+    });
+
+   
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+   
+
+   
+
+   
+    
+   
+    
+
+
+
+   
+
+
+
+
+
+    
+
+
+
+
+
+
+
+    
+
+
+
+
+   
+
+
+
+})(jQuery);
